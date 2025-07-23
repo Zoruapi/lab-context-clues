708dd1d621e54da04941b8f309b615379b8c9a83
Zoruapi <zorua.jian.jun@gmail.com>
2025-07-23 17:14:43 +0100

    Making design of webpage

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+/* 
+Accusations desgin
+
+<div class="col-6 col-md-4 col-lg-2">
+    <div class="bg-secondary-subtle text-secondary-emphasis rounded shadow-sm">
+        <h3 class="text-center">Accusation 1</h3>
+    </div>
+</div> */
